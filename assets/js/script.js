f5461c871e3218afad3b380f2071f2fb05ed46a7
Emanuele Vaccaro <evaccaro.001@gmail.com>
2026-03-20 11:09:28 +0100

    add api

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=billie_elish')
+    .then(data => data.json() )
+    .then (data => {
+        console.log(data)
+    })
+    .catch(error => console.error('Errore', error))
